0675d4e3fe689ab50f157bc2761be0be61e3324a
Yash Lahase <yashlahase1234@gmail.com>
2023-12-03 10:38:00 +0530

    Navbar added

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+
+const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 50);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <nav className={`navbar ${scrolled ? 'scrolled' : ''}`}>
+      <div className="nav-container">
+        <div className="nav-logo">YL</div>
+        <div className="nav-links">
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#projects">Projects</a>
+          <a href="#skills">Skills</a>
+          <a href="#contact">Contact</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
