c984bb9dd0d9d048bf5cd00c64c82a02036da7aa
Yash Lahase <yashlahase1234@gmail.com>
2023-12-03 10:30:00 +0530

    add custom cursor with hover effects and hero layout

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import './style.css';
+import Hero from './components/Hero';
+
+const App = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [cursorPos, setCursorPos] = useState({ x: 0, y: 0 });
+  const [isHovering, setIsHovering] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(true);
+
+    const handleMouseMove = (e) => {
+      setCursorPos({ x: e.clientX, y: e.clientY });
+    };
+
+    const handleMouseEnter = () => setIsHovering(true);
+    const handleMouseLeave = () => setIsHovering(false);
+
+    document.addEventListener('mousemove', handleMouseMove);
+
+    const interactiveElements = document.querySelectorAll('button, a');
+    interactiveElements.forEach(el => {
+      el.addEventListener('mouseenter', handleMouseEnter);
+      el.addEventListener('mouseleave', handleMouseLeave);
+    });
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+      interactiveElements.forEach(el => {
+        el.removeEventListener('mouseenter', handleMouseEnter);
+        el.removeEventListener('mouseleave', handleMouseLeave);
+      });
+    };
+  }, []);
+
+  return (
+    <div className={`app ${isLoaded ? 'loaded' : ''}`}>
+      {/* Custom Cursor */}
+      <div 
+        className={`cursor ${isHovering ? 'hover' : ''}`}
+        style={{
+          left: `${cursorPos.x}px`,
+          top: `${cursorPos.y}px`
+        }}
+      />
+      <div 
+        className="cursor-trail"
+        style={{
+          left: `${cursorPos.x}px`,
+          top: `${cursorPos.y}px`
+        }}
+      />
+
+      {/* Only Hero Component */}
+      <Hero />
+    </div>
+  );
+};
+
+export default App;
