289c2dc569277634d159baf88082c441181f8a2d
Yash Lahase <yashlahase1234@gmail.com>
2025-12-04 11:00:00 +0530

    Add Robotics Experience section component

---
+++ src/components/Robotics.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Robotics = () => {
+  return (
+    <section className="robotics">
+      <div className="container">
+        <h2 className="section-title">Robotics Experience</h2>
+        <div className="exp-card">
+          <div className="exp-icon">
+            <i className="fas fa-robot"></i>
+          </div>
+          <div className="exp-content">
+            <h3>Robotics Club</h3>
+            <h4>Line Following Robot Project</h4>
+            <ul>
+              <li>Circuit design</li>
+              <li>Microcontroller programming</li>
+              <li>Problem-solving</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Robotics;
